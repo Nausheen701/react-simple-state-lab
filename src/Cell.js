430d7bb87c02f1fe548637403efdfb93270fcc3e
Nausheen Akhter <nausheen701@gmail.com>
2022-02-11 12:50:27 +0300

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Cell extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            color:  props.value
+        }
+    }
+
+    // change color on click
+    handleClick = () => {
+        this.setState({
+          color: '#333'
+        })
+      }
+
+
+    render() {
+        return (
+            <div 
+            className='cell' 
+            style={{backgroundColor: this.state.color}}
+            onClick={this.handleClick}>
+
+            </div>
+            // Create a click listener which, when activated, updates the state 
+            // to the following hex value: '#333'
+        )
+    }
+}
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import Cell from './Cell.js'
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+  constructor(props) {
+    super()
+    this.state = {
+        values : [
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+          ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+        ]
+        
+    }
   }
   
-  genMatrix = () => {
+  genRow = (vals) => { 
+    return vals.map(val => <div className="cell"><Cell value={val} /></div>) // replace me and render a cell component instead!
+  }
+  
+  genMatrix = () => { 
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    
   }
   
   render() {
@@ -18,4 +38,12 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+// set 10 columns of 10 red squares
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
 }
